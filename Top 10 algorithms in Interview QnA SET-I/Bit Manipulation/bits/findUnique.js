5de31097a39e1826a1e9095d65a087d481377056
piyushshri01 <piyushshrimal94@gmail.com>
2020-10-10 00:27:50 +0530

    "Find Unique sol correction"

--- Top 10 algorithms in Interview QnA SET-I/Bit Manipulation/bits/findUnique.js
+++ Top 10 algorithms in Interview QnA SET-I/Bit Manipulation/bits/findUnique.js
@@ -1,22 +1,29 @@
 function findUnique(arr){
-    let prevXOR = 0;
-    let res = 0;
+    let resXOR = 0;
     for(let i = 0; i < arr.length; i++){
-        prevXOR = prevXOR ^ arr[i];
+        resXOR = resXOR ^ arr[i];
     }
 
-    if(prevXOR&1){
-        for(let i = 0; i < arr.length; i++){
-            if(arr[i]&1) res = res ^ arr[i];
+    let j = 0;
+    let temp = resXOR;
+    while(temp > 0){
+        if(temp&1){
+            break;
         }
-    } else {
-        for(let i = 0; i < arr.length; i++){
-            if((arr[i]&1) === 0) res = res ^ arr[i];
+        j++;
+        temp = temp>>1;
+    }
+
+    let mask = (1 << j);
+    let firstNo = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if((mask & arr[i]) != 0){
+            firstNo = firstNo^arr[i]
         }
     }
-    console.log(res);
-    console.log(res^prevXOR);
+    let secondNo = resXOR^firstNo;
+    console.log(firstNo, secondNo);
 }
 let arr = [1,3,5,2,6,1,3,2];
 arr = [1,1,2,5,7,2] // 01, 01, 10, 101, 111, 10
-console.log(findUnique(arr));
+findUnique(arr);
